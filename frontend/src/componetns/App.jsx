6ca953558785ef307a167d3288d4914c5d8b1fae
MixFix7 <mashtalar95@gmail.com>
2023-06-14 22:39:50 +0300

    update Map

---
+++ frontend/src/componetns/App.jsx
@@ -0,0 +1,53 @@
+import './../css/App.css';
+import axios from 'axios';
+import { Component } from 'react';
+import Map from './Home/Map';
+// import Map from './screen/map';
+import Footer from './Home/Footer';
+
+class App extends Component{
+  state = { details: [], }
+
+  componentDidMount() {
+    this.fetchData();
+
+    this.interval =  setInterval(() => this.fetchData(), 15000);
+  };
+
+  componentWillUnmount () {
+    clearInterval(this.interval);
+  };
+
+  fetchData() {
+    axios.get("http://localhost:8000/api/ukraineApi/")
+    .then(res => {
+      const data = res.data;
+      console.log(data)
+      this.setState({
+        details: data
+      });
+    })
+    .catch(err => {
+      console.error(err);
+  })};
+  
+  
+  render() {
+    return (
+      <div>
+        <Map className='fill-black'/>
+        <Footer/>
+
+      </div>
+
+      
+       
+    );
+  };
+
+};
+
+
+
+export default App;
+
---
+++ frontend/src/componetns/Home/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-600 flex justify-center">
+        <h1 className='text-center items-center text-4xl'>Ukraine Alarms Map</h1>
+    </footer>
+  )
+}
+
+export default Footer
